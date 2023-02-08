af1ef76624e28f3eed8839cd437a0b69d03d1077
Acer2 <Acer2>
2023-02-08 17:09:50 -0500

    add encoders

--- src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
+++ src/main/java/frc/robot/subsystems/ElevatorSubsystem.java
@@ -5,13 +5,18 @@
 package frc.robot.subsystems;
 
 import com.revrobotics.CANSparkMax;
+import com.revrobotics.RelativeEncoder;
+// import com.revrobotics.SparkMaxAbsoluteEncoder;
+// import com.revrobotics.SparkMaxRelativeEncoder;
 
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 // import edu.wpi.first.wpilibj.motorcontrol.Spark;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class ElevatorSubsystem extends SubsystemBase {
   /** Creates a new ElevatorSubsystem. */
   private final CANSparkMax elevatorMotor = new CANSparkMax(1, CANSparkMax.MotorType.kBrushless); // How is this working
+  private final RelativeEncoder elevatorEncoder = elevatorMotor.getEncoder();
   // private final 
 
     public ElevatorSubsystem() {}
@@ -24,5 +29,6 @@ public class ElevatorSubsystem extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
+    SmartDashboard.putNumber("Elevator Encoder", elevatorEncoder.getPosition());
   }
 }
