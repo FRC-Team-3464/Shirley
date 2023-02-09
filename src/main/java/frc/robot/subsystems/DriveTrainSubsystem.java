502f41932fbed0a72a25ea5ccc1a97bc1206cdac
Acer2 <Acer2>
2023-02-09 17:02:10 -0500

    Finished Tank Drive, started Arcade

--- src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
+++ src/main/java/frc/robot/subsystems/DriveTrainSubsystem.java
@@ -7,6 +7,7 @@ package frc.robot.subsystems;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 
+import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class DriveTrainSubsystem extends SubsystemBase {
@@ -15,7 +16,6 @@ public class DriveTrainSubsystem extends SubsystemBase {
   // Defining the drive train motors
   
   // Motor Ports will be changed later
-  
   private final CANSparkMax
     leftFront = new CANSparkMax(3, null),
     leftBack = new CANSparkMax(4, null),
@@ -26,8 +26,44 @@ public class DriveTrainSubsystem extends SubsystemBase {
   private final RelativeEncoder leftFrontEncoder = leftFront.getEncoder();
   private final RelativeEncoder rightFrontEncoder = rightFront.getEncoder();
 
+  // Differential drive, allows arcade drive and tank drive
+  public DifferentialDrive drive = new DifferentialDrive(leftFront, rightFront);
+
+
+
   
-  public DriveTrainSubsystem() {}
+  public DriveTrainSubsystem() {
+    // Inverts the left motor, allowing it to go straight
+    leftFront.setInverted(true);
+  }
+
+  public void driveTank(double left, double right) {
+    // Gets rid of the joystick drift
+    if (Math.abs(left) < 0.07) {
+      left = 0;
+    }
+    if (Math.abs(right) < 0.07) {
+      right = 0;
+    }
+
+    // Uses the existing tankDrive() from the DifferentialDrive along with modified values
+    drive.tankDrive(left, right);
+
+    // Makes the back motors move the same speed as the front
+    leftBack.set(leftFront.get());
+    rightBack.set(rightFront.get());
+  }
+
+  public void arcadeDrive(double speed, double rotation) {
+    // Uses the built in arcadeDrive() from DifferentialDrive
+    drive.arcadeDrive(speed, rotation);
+    // Have back motors follow the front motors
+    leftBack.set(leftFront.get());
+    rightBack.set(rightFront.get());
+  }
+
+
+
 
   @Override
   public void periodic() {
