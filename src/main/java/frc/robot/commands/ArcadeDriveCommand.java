50532314bff9ddb0c62e3fb88f928a2f7d44705c
Yifinity <92883414+Yifinity@users.noreply.github.com>
2023-02-18 12:53:26 -0500

    clean up arcade drive

--- src/main/java/frc/robot/commands/ArcadeDriveCommand.java
+++ src/main/java/frc/robot/commands/ArcadeDriveCommand.java
@@ -4,7 +4,7 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.Joystick;
+
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
