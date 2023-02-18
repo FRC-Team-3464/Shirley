4cd1a0aa36ae86edd9e59191eec9d966145bcf63
Yifinity <92883414+Yifinity@users.noreply.github.com>
2023-02-18 15:43:14 -0500

    Also rename pid controller

--- src/main/java/frc/robot/commands/PivoterPIDCommand.java
+++ src/main/java/frc/robot/commands/PivoterPIDCommand.java
@@ -14,15 +14,15 @@ public class PivoterPIDCommand extends CommandBase {
   private final PivoterSubsystem pivoterSub;
 
   // private final PIDController armPIDController = new PIDController(0.00555555555, 0, 0);
-  private final PIDController armPIDController = new PIDController(0.00455555555, 0, 0);
+  private final PIDController pivotPIDController = new PIDController(0.01111111111, 0, 0);
   private double speed;
 
   public PivoterPIDCommand(PivoterSubsystem pivoterSubsystem, double target) {
     // Use addRequirements() here to declare subsystem dependencies.
     pivoterSub = pivoterSubsystem;
     addRequirements(pivoterSubsystem);
-    armPIDController.setSetpoint(target);
-    armPIDController.setTolerance(1);
+    pivotPIDController.setSetpoint(target);
+    pivotPIDController.setTolerance(15); // Change tolarance
   }
 
   // Called when the command is initially scheduled.
@@ -32,15 +32,13 @@ public class PivoterPIDCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    speed = armPIDController.calculate(pivoterSub.getPivoterDegrees()); // This should help. 
+    speed = pivotPIDController.calculate(pivoterSub.getPivoterDegrees()); // This should help. 
     if(speed > 0.35){
       speed = 0.35;
     }
     pivoterSub.pivot(speed); // This doesn't seem to be working
-    SmartDashboard.putNumber("Pivoter Error", armPIDController.getPositionError());
-    SmartDashboard.putBoolean("Pivot Command Complete:" , armPIDController.atSetpoint());
-    SmartDashboard.putNumber("Pivot Speed", speed);
-    
+    SmartDashboard.putNumber("Pivoter Error", pivotPIDController.getPositionError());
+    SmartDashboard.putBoolean("Pivot Command Complete:" , pivotPIDController.atSetpoint());    
   }
 
   // Called once the command ends or is interrupted.
