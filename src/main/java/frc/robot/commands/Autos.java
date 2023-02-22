6e0d5b9bd5070ee72dc0312a8094b6d3b1d31353
Yifinity <yifan99.jiang@gmail.com>
2023-02-21 22:40:14 -0500

    add helpful documentation

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -3,7 +3,7 @@
 // // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
+// HUH?
 // import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
