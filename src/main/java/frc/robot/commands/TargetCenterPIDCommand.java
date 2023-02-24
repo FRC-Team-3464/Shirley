aab1c7bc2b29e84b05c80c2dac7e65c314564b32
Legion <emmarydzik@gmail.com>
2023-02-24 15:46:22 -0500

    Make modifications to TargetPID and Target Range

--- src/main/java/frc/robot/commands/TargetCenterPIDCommand.java
+++ src/main/java/frc/robot/commands/TargetCenterPIDCommand.java
@@ -9,34 +9,26 @@ import org.photonvision.PhotonCamera;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
-import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.PhotonVisionSubsystem;
-import frc.robot.subsystems.PivoterSubsystem;
 
-public class TargetPIDCommand extends CommandBase {
-  /** Creates a new SetGrabberCommand. */
-  //private final PivoterSubsystem pivoterSub;
+public class TargetCenterPIDCommand extends CommandBase {
   private final PhotonVisionSubsystem photonSub;
   private final DrivetrainSubsystem driveSub;
 
-  // private final PIDController armPIDController = new PIDController(0.00555555555, 0, 0);
-  //private final PIDController pivotPIDController = new PIDController(0.01111111111, 0, 0);
-
-  private double speed;
   // Constants such as camera and target height stored. Change per robot and goal!
   final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
   final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
+
   // Angle between horizontal and the camera.
   final double CAMERA_PITCH_RADIANS = Units.degreesToRadians(0);
 
   // How far from the target we want to be
   final double GOAL_RANGE_METERS = Units.feetToMeters(3);
 
-  // PID constants should be tuned per robot
+// PID constants should be tuned per robot - GET THIS UPDATED
   final double LINEAR_P = 0.1;
   final double LINEAR_D = 0.0;
   PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
@@ -53,63 +45,49 @@ public class TargetPIDCommand extends CommandBase {
   private double rotationSpeed;
   
 
-  public TargetPIDCommand(PhotonVisionSubsystem photon, DrivetrainSubsystem drive) {
+  public TargetCenterPIDCommand(PhotonVisionSubsystem photon, DrivetrainSubsystem drive) {
     // Use addRequirements() here to declare subsystem dependencies.
     photonSub = photon;
     driveSub = drive;
+
+    camera = photonSub.getColorCamera();  
     addRequirements(driveSub);
     addRequirements(photonSub);
     
-
-    forwardController.setSetpoint(0);
-
-    turnController.setSetpoint(0);
-  
-    camera = photonSub.getColorCamera();  
   }
 
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    forwardController.setSetpoint(0);
+    turnController.setSetpoint(0);
     
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+
+    forwardSpeed = controller.getLeftY();
+    // Vision-alignment mode
+    // Query the latest result from PhotonVision
+    var result = camera.getLatestResult();
+
+    if (result.hasTargets()) {
+        // Calculate angular turn power
+        // -1.0 required to ensure positive PID controller effort _increases_ yaw
+        rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
+      } 
+    else{
+      rotationSpeed = 0;
+    }
     
-    //speed = pivotPIDController.calculate(pivoterSub.getPivoterDegrees()); // This should help. 
-    //if(speed > 0.35){
-      //speed = 0.35;
-    //}
-    //pivoterSub.pivot(speed); // This doesn't seem to be working
-    //SmartDashboard.putNumber("Pivoter Error", pivotPIDController.getPositionError());
-    //SmartDashboard.putBoolean("Pivot Command Complete:" , pivotPIDController.atSetpoint());   
-    
-    
-
-        forwardSpeed = controller.getLeftY();
-
-        // Vision-alignment mode
-        // Query the latest result from PhotonVision
-        var result = camera.getLatestResult();
-
-        if (result.hasTargets()) {
-            // Calculate angular turn power
-            // -1.0 required to ensure positive PID controller effort _increases_ yaw
-            rotationSpeed = -turnController.calculate(result.getBestTarget().getYaw(), 0);
-          } 
-        else{
-          rotationSpeed = 0;
-        }
-        
-        if(rotationSpeed < .2 && rotationSpeed > -.2) {
-             // If we have no targets, stay still.
-            rotationSpeed = 0;
-        }
-
-        // Use our forward/turn speeds to control the drivetrain
-        driveSub.arcadeDrive(forwardSpeed, rotationSpeed);
+    if(rotationSpeed < .2 && rotationSpeed > -.2) {
+      // If the rotation speed is too small - set it to zero.
+        rotationSpeed = 0;
+    }
+    // Use our forward/turn speeds to control the drivetrain
+    driveSub.arcadeDrive(forwardSpeed, rotationSpeed);
   }
 
   // Called once the command ends or is interrupted.
--- src/main/java/frc/robot/commands/TargetRangePIDCommand.java
+++ src/main/java/frc/robot/commands/TargetRangePIDCommand.java
@@ -10,24 +10,16 @@ import org.photonvision.PhotonUtils;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
-import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.subsystems.DrivetrainSubsystem;
 import frc.robot.subsystems.PhotonVisionSubsystem;
-import frc.robot.subsystems.PivoterSubsystem;
 
 public class TargetRangePIDCommand extends CommandBase {
-  /** Creates a new SetGrabberCommand. */
-  //private final PivoterSubsystem pivoterSub;
+
   private final PhotonVisionSubsystem photonSub;
   private final DrivetrainSubsystem driveSub;
 
-  // private final PIDController armPIDController = new PIDController(0.00555555555, 0, 0);
-  //private final PIDController pivotPIDController = new PIDController(0.01111111111, 0, 0);
-
-  private double speed;
   // Constants such as camera and target height stored. Change per robot and goal!
   final double CAMERA_HEIGHT_METERS = Units.inchesToMeters(24);
   final double TARGET_HEIGHT_METERS = Units.feetToMeters(5);
@@ -37,7 +29,7 @@ public class TargetRangePIDCommand extends CommandBase {
   // How far from the target we want to be
   final double GOAL_RANGE_METERS = Units.feetToMeters(3);
 
-  // PID constants should be tuned per robot
+  // PID constants should be tuned per robot - needs to be changed
   final double LINEAR_P = 0.1;
   final double LINEAR_D = 0.0;
   PIDController forwardController = new PIDController(LINEAR_P, 0, LINEAR_D);
@@ -47,7 +39,6 @@ public class TargetRangePIDCommand extends CommandBase {
   PIDController turnController = new PIDController(ANGULAR_P, 0, ANGULAR_D);
 
   private final XboxController controller = OI.xBoxController;
-  
   private final PhotonCamera camera;
 
   private double forwardSpeed;
@@ -58,30 +49,24 @@ public class TargetRangePIDCommand extends CommandBase {
     // Use addRequirements() here to declare subsystem dependencies.
     photonSub = photon;
     driveSub = drive;
-    addRequirements(driveSub);
-    addRequirements(photonSub);
-    
-
-    //forwardController.setSetpoint(0);
-
-    //turnController.setSetpoint(0);
-  
     camera = photonSub.getColorCamera();  
+    addRequirements(driveSub);
+    addRequirements(photonSub);    
   }
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    
+  public void initialize() {  
+    // Reset the controllers. 
+    forwardController.setSetpoint(0);
+    turnController.setSetpoint(0); 
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-  
+    //  This does not make sense - in what scenario will we be moving towards the target but also being able to free up the turn value?
         rotationSpeed = controller.getLeftX();
-
-        // Vision-alignment mode
         // Query the latest result from PhotonVision
         var result = camera.getLatestResult();
 
@@ -93,11 +78,9 @@ public class TargetRangePIDCommand extends CommandBase {
                     TARGET_HEIGHT_METERS,
                     CAMERA_PITCH_RADIANS,
                     Units.degreesToRadians(result.getBestTarget().getPitch()));
-
             // Use this range as the measurement we give to the PID controller.
             // -1.0 required to ensure positive PID controller effort _increases_ range
             forwardSpeed = -forwardController.calculate(range, GOAL_RANGE_METERS);
-
 
             // Also calculate angular power
             // -1.0 required to ensure positive PID controller effort _increases_ yaw
