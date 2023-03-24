b71cc470337da98e5599aeb383a30271605f671e
Legion <yifan99.jiang@gmail.com>
2023-03-24 18:10:21 -0400

    add LED green to end behavior

--- src/main/java/frc/robot/commands/AutoFeederDistance.java
+++ src/main/java/frc/robot/commands/AutoFeederDistance.java
@@ -9,6 +9,7 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
 import frc.robot.subsystems.DrivetrainSubsystem;
+import frc.robot.subsystems.LEDSubsystem;
 import frc.robot.subsystems.UltrasonicSubsystem;
 
 public class AutoFeederDistance extends CommandBase {
@@ -17,13 +18,15 @@ public class AutoFeederDistance extends CommandBase {
  
   private final DrivetrainSubsystem drivetrain;
   private final UltrasonicSubsystem ultrasonic;
+  private final LEDSubsystem led;
   private double target;
 
-  public AutoFeederDistance(DrivetrainSubsystem drivetrainSub, UltrasonicSubsystem ultrasonicSub, double target) {
+  public AutoFeederDistance(DrivetrainSubsystem drivetrainSub, UltrasonicSubsystem ultrasonicSub, LEDSubsystem ledSub, double target) {
     drivetrain = drivetrainSub;
     ultrasonic = ultrasonicSub;
+    led = ledSub;
     this.target = target;
-    addRequirements(drivetrain, ultrasonic);
+    addRequirements(drivetrain, ultrasonic, led);
     // Use addRequirements() here to declare subsystem dependencies.
   }
 
@@ -34,13 +37,16 @@ public class AutoFeederDistance extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.arcadeDrive(-0.2, controller.getRightX());
+    led.red();
+    drivetrain.arcadeDrive(-0.3, controller.getRightX());
+
     // SmartDashboard.
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
+    led.green();
     drivetrain.stopDrive();
   }
 
