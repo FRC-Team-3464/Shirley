9c5d34c39a5951a95afab46e5eddacba41d3f5a7
Acer1 <Acer1>
2023-03-24 15:07:46 -0400

    add controller control for autofeederDistance

--- src/main/java/frc/robot/commands/AutoFeederDistance.java
+++ src/main/java/frc/robot/commands/AutoFeederDistance.java
@@ -5,7 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj.XboxController;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+// import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.OI;
 import frc.robot.subsystems.DrivetrainSubsystem;
@@ -34,7 +34,7 @@ public class AutoFeederDistance extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    drivetrain.arcadeDrive(-0.2, controller.getRightX());
+    drivetrain.arcadeDrive(-controller.getLeftTriggerAxis(), controller.getRightX());
     // SmartDashboard.
   }
 
